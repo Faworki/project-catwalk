07071d4414cdd5ac422ef0470603729830424e23
Colin Chauche <colin.chauche@gmail.com>
2021-01-13 18:16:52 -0500

    Add setupTests.js file and refactor App.test.js

--- client/components/App.test.js
+++ client/components/App.test.js
@@ -1,11 +1,9 @@
 // This is just a simple example test file that can be modified later or deleted.
 
 import React from 'react'; // Test files must import react
-import Adapter from 'enzyme-adapter-react-16';
-import {shallow, configure} from 'enzyme'; // import any functions you are using from enzyme
+import {shallow} from 'enzyme'; // import any functions you are using from enzyme
 import App from './App.jsx'; // import what ever component you are testing.
 
-configure({adapter: new Adapter()});
 
 test('should render with text "Hello, World"', () => {
   // Render a app with label in the document

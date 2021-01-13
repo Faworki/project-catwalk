46b6464d374a058a7ca992fe92342f3368a2e3ec
Raymond Anthony Hunce <hunce18c@gmail.com>
2021-01-12 21:46:39 -0500

    Create app.jsx

---
+++ client/components/app.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class App extends React.Component {
+  constructor() {
+    super();
+  }
+
+  render () {
+    return (
+      <div>
+        Hello, World!
+      </div>
+    )
+  }
+  
+}
+
+export default App;

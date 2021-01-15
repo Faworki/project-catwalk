f78b593c391f43d69b5ef5bfe13c5b2487a29904
Zach Realberg <zrealberg@gmail.com>
2021-01-15 15:44:51 -0500

    added .gitignore file

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,28 +1,41 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2020": true,
-        "node": true
-    },
-    "settings": {
-        "react": {
-          "version": "^16.13.1"
-        }
-      },
-    "extends": [
-        "eslint:recommended",
-        "plugin:react/recommended"
-    ],
-    "parserOptions": {
-        "ecmaFeatures": {
-            "jsx": true
-        },
-        "ecmaVersion": 11,
-        "sourceType": "module"
-    },
-    "plugins": [
-        "react"
-    ],
-    "rules": {
+env: {
+    'es6': true
+},
+parserOptions: {
+    'sourceType': 'module',
+    ecmaFeatures: {
+    'jsx': true
+    }
+},
+
+rules: {
+    /* Indentation */
+    'no-mixed-spaces-and-tabs': 2,
+    // 'indent': [2],
+    /* Variable cames */
+    'camelcase': 2,
+    /* Language constructs */
+    'curly': 2,
+    'eqeqeq': [2, 'smart'],
+    'func-style': [2, 'expression'],
+    /* Semicolons */
+    'semi': 2,
+    'no-extra-semi': 2,
+    /* Padding & additional whitespace (perferred but optional) */
+    'brace-style': [2, '1tbs', { 'allowSingleLine': true }],
+    'semi-spacing': 1,
+    'key-spacing': 1,
+    'block-spacing': 1,
+    'comma-spacing': 1,
+    'no-multi-spaces': 1,
+    'space-before-blocks': 1,
+    'keyword-spacing': [1, { 'before': true, 'after': true }],
+    'space-infix-ops': 1,
+    /* Variable declaration */
+    'one-var': [1, { 'uninitialized': 'always', 'initialized': 'never' }],
+    /* Minuta */
+    'comma-style': [2, 'last'],
+    'quotes': [1, 'single']
     }
 };

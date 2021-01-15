43f09b2c5c5fd73c82b0d4469c89c50516e02a43
Colin Chauche <colin.chauche@gmail.com>
2021-01-15 17:14:08 -0500

    Add authentication headers to proxy requests to Atelier

--- server/index.js
+++ server/index.js
@@ -1,14 +1,21 @@
 const express = require('express');
 const path = require('path');
 const proxy = require('express-http-proxy');
+const API_TOKEN = process.env.API_TOKEN || require('./config')['API_TOKEN'];
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 const ATELIER_HOST = 'https://app-hrsei-api.herokuapp.com';
-// const API_KEY;
+
+const proxyOptions = {
+  proxyReqOptDecorator: (proxyReqOpts, srcReq) => {
+    proxyReqOpts.headers['Authorization'] = API_TOKEN;
+    return proxyReqOpts
+  }
+};
 
 app.use(express.static(path.join(__dirname, '../public')));
-app.use('/', proxy(ATELIER_HOST));
+app.use('/', proxy(ATELIER_HOST, proxyOptions));
 
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);

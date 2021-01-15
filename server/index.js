9e4c43af89b55bbbc311267032b041b4b8cef933
Colin Chauche <colin.chauche@gmail.com>
2021-01-15 16:33:31 -0500

    Add express-http-proxy to server

--- server/index.js
+++ server/index.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const path = require('path');
-
+const proxy = require('express-http-proxy');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+const ATELIER_HOST = 'https://app-hrsei-api.herokuapp.com';
+// const API_KEY;
 
 app.use(express.static(path.join(__dirname, '../public')));
+app.use('/', proxy(ATELIER_HOST));
 
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);

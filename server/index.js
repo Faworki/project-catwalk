0b745fe2413b4334b0e094237db6a7cb7cd2026c
Colin Chauche <colin.chauche@gmail.com>
2021-01-15 17:36:38 -0500

    Refactor server files for easier testing and better modularity

--- server/index.js
+++ server/index.js
@@ -1,21 +1,5 @@
-const express = require('express');
-const path = require('path');
-const proxy = require('express-http-proxy');
-const API_TOKEN = process.env.API_TOKEN || require('./config')['API_TOKEN'];
-
-const app = express();
+const app = require('./server.js');
 const PORT = process.env.PORT || 3000;
-const ATELIER_HOST = 'https://app-hrsei-api.herokuapp.com';
-
-const proxyOptions = {
-  proxyReqOptDecorator: (proxyReqOpts, srcReq) => {
-    proxyReqOpts.headers['Authorization'] = API_TOKEN;
-    return proxyReqOpts
-  }
-};
-
-app.use(express.static(path.join(__dirname, '../public')));
-app.use('/', proxy(ATELIER_HOST, proxyOptions));
 
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
---
+++ server/server.test.js
@@ -0,0 +1,7 @@
+describe("Server", () => {
+  it('should return index.html from "/" endpoint', async (done) => {
+
+  });
+
+  it('requests to "/api/fec2/hrnyc/" should return data from Atlier API', () => {});
+});

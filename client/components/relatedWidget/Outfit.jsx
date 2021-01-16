6ffe99443aa5a08b815efbd53d2830b46431b0d9
Jimmy Ko <ko.jimmy09@gmail.com>
2021-01-16 15:40:02 -0500

    created outfit component file

---
+++ client/components/relatedWidget/Outfit.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class Outfit extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        Outfit Carousel
+      </div>
+    );
+  }
+}
+
+export default Outfit;

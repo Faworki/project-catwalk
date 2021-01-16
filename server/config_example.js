1cb1973435e70708bfeb316a6d02523a517560b2
Colin Chauche <colin.chauche@gmail.com>
2021-01-16 14:06:30 -0500

    Restore config_example.js

---
+++ server/config_example.js
@@ -0,0 +1,6 @@
+// Make a copy of this file and rename it to 'config.js'
+// Replace '<Auth Token>' with your gitHub authorization token string
+// Remember not to commit the file with your authorization token
+module.exports = {
+  API_TOKEN: '<Auth Token>'
+}

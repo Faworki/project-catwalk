44eef665a0ebaf9f426ff5566f2026102171afe6
Jimmy Ko <ko.jimmy09@gmail.com>
2021-01-16 15:43:18 -0500

    created Related Products Carousel file

---
+++ client/components/relatedWidget/RelatedCarousel.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class RelatedCarousel extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        Related Products Carousel
+      </div>
+    );
+  }
+}
+
+export default RelatedCarousel;

a6d1592ce707783fb00144d5ba35dca34bf2a65d
Jimmy Ko <ko.jimmy09@gmail.com>
2021-01-16 18:10:36 -0500

    built skeleton for button

---
+++ client/components/relatedWidget/ActionButton.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class ActionButton extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        ActionButton
+      </div>
+    );
+  }
+}
+
+export default ActionButton;

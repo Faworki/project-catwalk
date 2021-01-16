0a291bea1b89668e480935ac00c1c0e0cb3c6c49
Colin Chauche <colin.chauche@gmail.com>
2021-01-16 11:32:59 -0500

    Add class component

---
+++ client/components/reviewModule/ReviewsWidget.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+export class ReviewsWidget extends Component {
+  render() {
+    return (
+      <div>
+        ReviewsWidget
+      </div>
+    )
+  }
+}
+
+export default ReviewsWidget
+

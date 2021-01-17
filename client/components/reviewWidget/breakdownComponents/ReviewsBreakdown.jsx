de6bca29b6c88a53bd47a9b456550b46f91d73a3
Colin Chauche <colin.chauche@gmail.com>
2021-01-17 17:47:15 -0500

    Remove conditional rendering now that props are defined

--- client/components/reviewWidget/breakdownComponents/ReviewsBreakdown.jsx
+++ client/components/reviewWidget/breakdownComponents/ReviewsBreakdown.jsx
@@ -1,18 +1,18 @@
-import React from "react";
-import ReviewBar from "./ReviewBar";
+import React from 'react';
+import ReviewBar from './ReviewBar';
 
-// Todo: Find a cleaner way to do these calculations.
 const ReviewsBreakdown = ({ ratings }) => {
   let bars = [];
-  if (ratings) { // There must be a way to not have to do this
-    let totalRatings = Object.values(ratings).reduce((sum, num) => sum + parseInt(num), 0);
-    for (let i = 0; i < 6; i++) {
-      let numRatings = ratings.hasOwnProperty(i) ? ratings[i] : 0;
-      let percentOfRatings = Math.trunc((numRatings / totalRatings) * 100);
-      bars.push(
-        <ReviewBar key={i} starNum={i} percentOfRatings={percentOfRatings} />
-      );
-    }
+  let totalRatings = Object.values(ratings).reduce(
+    (sum, num) => sum + parseInt(num),
+    0
+  );
+  for (let i = 0; i < 6; i++) {
+    let numRatings = ratings.hasOwnProperty(i) ? ratings[i] : 0;
+    let percentOfRatings = Math.trunc((numRatings / totalRatings) * 100);
+    bars.push(
+      <ReviewBar key={i} starNum={i} percentOfRatings={percentOfRatings} />
+    );
   }
 
   return <section>{bars}</section>;

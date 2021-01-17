ec197945751346e1fe278820a1849bba7191d1f5
Colin Chauche <colin.chauche@gmail.com>
2021-01-17 17:53:58 -0500

    Add key to characteristic labels

--- client/components/reviewWidget/breakdownComponents/CharacteristicScale.jsx
+++ client/components/reviewWidget/breakdownComponents/CharacteristicScale.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 
-const CharacteristicScale = ({valuePercent, name, labels}) => {
+const CharacteristicScale = ({ valuePercent, name, labels }) => {
   return (
     <article className="char-scale">
       <h5>{name}</h5>
       <div>{valuePercent + '%'}</div>
-      {labels.map((label) => (<h6>{label}</h6>))}
+      {labels.map((label) => (
+        <h6 key={name + label}>{label}</h6>
+      ))}
     </article>
   );
 };

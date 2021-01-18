a36b98311ae57be8311a6a9f39ae29a4f3b9a253
Colin Chauche <colin.chauche@gmail.com>
2021-01-18 12:33:54 -0500

    Add dynamic data rendering to StarAverage

--- client/components/shared/StarAverage.jsx
+++ client/components/shared/StarAverage.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
+/**
+ * This component expects a prop called 'reviewAverage'
+ * that sould be a number between 0 and 5.
+ *
+ * Ex: <StarAverage reviewAverage={3.5}
+ *     <StarAverage reviewAverage={this.props.reviewAverage}
+ * */
 
-const StarAverage = () => {
+const StarAverage = ({reviewAverage}) => {
   return (
     <div className="star-container">
-      StarAverage
+      {reviewAverage + ' Stars'}
     </div>
   );
 };

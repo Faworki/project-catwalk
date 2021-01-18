3ebbd70a87a8dbfe056f75117e4c2f25cd4e88a5
Jimmy Ko <ko.jimmy09@gmail.com>
2021-01-18 16:07:40 -0500

    commented out Outfits Component

--- client/components/RelatedProducts.jsx
+++ client/components/RelatedProducts.jsx
@@ -9,44 +9,23 @@ class RelatedProducts extends React.Component {
 
     this.state = {
       relatedIDs: [],
-      relatedProducts: [{name: 'Product 1'}, {name: 'Product 2'}, {name: 'Product 3'}, {name: 'Product 4'}, {name: 'Product 5'}, {name: 'Product 6'}, {name: 'Product 7'}]
+      relatedProducts: []
     };
   }
 
   componentDidMount() {
     console.log('RELATED PRODUCTS COMPONENT MOUNTED!');
-    // axios.get('/api/fec2/hrnyc/products/11001/related')
-    // .then(results=>{
-    //   return results.data.map(relatedId=>{
-    //     return axios.get(`/api/fec2/hrnyc/products/${relatedId}/styles`);
-    //   });
-    // })
-    // .then(results=>{
-    //   Promise.all(results)
-    //   .then(results=>{
-    //     return results.map((res)=>{
-    //       return res.data;
-    //     });
-    //   })
-    //   .then(results=>{
-    //     this.setState({relatedProducts: results});
-    //   })
-    //   .catch(err=>{ console.log('Promise.all error'); });
-    // })
-    // .catch(err=>{
-    //   console.log('Related Products Error');
-    // });
   }
 
   render () {
     return (
       <div>
-        <strong>RELATED PRODUCTS</strong>
-        <RelatedCarousel/>
-        <Outfit
+        <strong>RELATED PRODUCTS WIDGET</strong>
+        <RelatedCarousel product={this.props.product}/>
+        {/* <Outfit
         yourOutfit={this.props.yourOutfit}
         addToOutfit={this.props.addToOutfit}
-        />
+        /> */}
       </div>
     );
   }

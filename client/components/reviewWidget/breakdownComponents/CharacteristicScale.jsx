16b545ad2069153359a40e95b26363ee1d4a9bc4
Colin Chauche <colin.chauche@gmail.com>
2021-01-19 17:55:00 -0500

    Render basic scale-bar

--- client/components/reviewWidget/breakdownComponents/CharacteristicScale.jsx
+++ client/components/reviewWidget/breakdownComponents/CharacteristicScale.jsx
@@ -4,10 +4,18 @@ const CharacteristicScale = ({ valuePercent, name, labels }) => {
   return (
     <article className="char-scale">
       <h5>{name}</h5>
-      <div>{valuePercent + '%'}</div>
-      {labels.map((label) => (
-        <h6 key={name + label}>{label}</h6>
-      ))}
+      <div className="scale">
+        <div className="scale-bar"></div>
+        <div className="scale-bar"></div>
+        <div className="scale-bar"></div>
+        <div className="scale-bar"></div>
+        <span className="pointer">▼</span>
+      </div>
+      <div>
+        {labels.map((label) => (
+          <h6 key={name + label}>{label}</h6>
+        ))}
+      </div>
     </article>
   );
 };

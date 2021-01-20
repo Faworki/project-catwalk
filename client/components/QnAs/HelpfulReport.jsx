ba189d8ea45c09174407b49b38d8b76498c26644
Zach Realberg <zrealberg@gmail.com>
2021-01-20 12:15:04 -0500

    adding styling and integrated it into conditional rendering for 'add answer' and 'report'

--- client/components/QnAs/HelpfulReport.jsx
+++ client/components/QnAs/HelpfulReport.jsx
@@ -66,19 +66,26 @@ class HelpfulReport extends React.Component {
   }
 
   render() {
+    //declaring a variable that will control the "report" text that should appear ONLY in answer HR instances
+    var answerText;
+    if(this.props.answerUsage) {
+      if(!this.state.reported){
+        answerText = <button className='reportAnswer' onClick={this.report}>{this.state.reportText}</button>;
+      } else {
+        answerText = <p className='reported'>{this.state.reportText}</p>;
+      }
+    }
+
     return (
       <div>
         <div>
           Helpful?
           <a onClick={this.clickHelpful}> | Yes ({this.state.helpVotes}) | </a>
-          {this.props.answerUsage && !this.state.reported ?
-          (<button onClick={this.report}>{this.state.reportText}</button>)
-          :
-          (<div>{this.state.reportText}</div>)
-          }
+          {answerText}
+          {/* this logic asks if this IS NOT an answer instance, i.e. is this for a question */}
           {!this.props.answerUsage && (
-            <div>
-              <a onClick={this.addAnswer}>Add Answer</a>
+            <div className='addAnswer'>
+              <button className='addAnswer' onClick={this.addAnswer}>Add Answer</button>
               <div>
                 {/* this modal is for submitting a new answer */}
                 <ModalComp

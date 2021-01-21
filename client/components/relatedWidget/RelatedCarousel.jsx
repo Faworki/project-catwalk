6809a159034173c5f455ac2a0f3fe5c3ed617cdf
Jimmy Ko <ko.jimmy09@gmail.com>
2021-01-21 02:04:41 -0500

    refactored GET requests to use async/await

--- client/components/relatedWidget/RelatedCarousel.jsx
+++ client/components/relatedWidget/RelatedCarousel.jsx
@@ -37,42 +37,42 @@ class RelatedCarousel extends React.Component {
     this.setState({ selected: key });
   }
 
-  getRelatedProduct(productId) {
-    axios.get(`/api/fec2/hrnyc/products/${productId}/related`)
-    .then(results=>{
-      this.setState({relatedIds: results.data});
-      let products = results.data.map(relatedId=>{
-        return axios.get(`/api/fec2/hrnyc/products/${relatedId}`);
-      });
-      let images = results.data.map(relatedId=>{
-        return axios.get(`/api/fec2/hrnyc/products/${relatedId}/styles`);
-      });
-      return {
-        products: products,
-        images: images
-      };
-    })
-    .then(results=>{
-          Promise.all(results.products)
-          .then(results=>{
-            return results.map((res)=>{
-              return res.data;
-            });
-          })
-          .then(results=>{ this.setState({relatedProducts: results}); })
-          .catch(err=>{ console.log(err); });
+  // getRelatedProduct(productId) {
+  //   axios.get(`/api/fec2/hrnyc/products/${productId}/related`)
+  //   .then(results=>{
+  //     // this.setState({relatedIds: results.data});
+  //     let products = results.data.map(relatedId=>{
+  //       return axios.get(`/api/fec2/hrnyc/products/${relatedId}`);
+  //     });
+  //     let images = results.data.map(relatedId=>{
+  //       return axios.get(`/api/fec2/hrnyc/products/${relatedId}/styles`);
+  //     });
+  //     return {
+  //       products: products,
+  //       images: images
+  //     };
+  //   })
+  //   .then(results=>{
+  //         Promise.all(results.products)
+  //         .then(results=>{
+  //           return results.map((res)=>{
+  //             return res.data;
+  //           });
+  //         })
+  //         .then(results=>{ this.setState({relatedProducts: results}); })
+  //         .catch(err=>{ console.log(err); });
 
-          Promise.all(results.images)
-          .then(results=>{
-            return results.map((res)=>{
-              return res.data.results[0].photos[0].thumbnail_url;
-            });
-          })
-          .then(results=>{ this.setState({relatedImages: results}); })
-          .catch(err=>{ console.log(err); });
-    })
-    .catch(err=>{ console.log(err); });
-  }
+  //         Promise.all(results.images)
+  //         .then(results=>{
+  //           return results.map((res)=>{
+  //             return res.data.results[0].photos[0].thumbnail_url;
+  //           });
+  //         })
+  //         .then(results=>{ this.setState({relatedImages: results}); })
+  //         .catch(err=>{ console.log(err); });
+  //   })
+  //   .catch(err=>{ console.log(err); });
+  // }
 
   buildCarousel() {
     this.productItems = new ProductList(
@@ -82,15 +82,120 @@ class RelatedCarousel extends React.Component {
       },
       this.state.selected,
       this.props.getNewProduct);
+
+      console.log('building carousel')
   }
 
-  componentDidMount() {
-    this.buildCarousel();
+  getRelatedIds(productId) {
+    axios.get(`/api/fec2/hrnyc/products/${productId}/related`)
+    .then(relatedProductIdArray=>{
+      this.setState({relatedIds: relatedProductIdArray.data});
+    })
+    .catch((err)=>{
+      console.log(err);
+    });
   }
 
-  componentDidUpdate(prevProps) {
+  getRelatedProducts(idArray) {
+    idArray.map(relatedId=>{
+      return axios.get(`/api/fec2/hrnyc/products/${relatedId}`);
+    })
+    .then(relatedProductPromiseArray=>{
+      relatedProductArray.map(relatedProduct=>{
+        return relatedProduct.data;
+      });
+    })
+    .then(relatedProductObjectArray=>{
+      this.setState({relatedProducts: relatedProductObjectArray});
+    })
+    .catch(err=>{
+      console.log(err);
+    });
+  }
+
+  getRelatedImages(idArray) {
+    idArray.map(relatedId=>{
+      return axios.get(`/api/fec2/hrnyc/products/${relatedId}/styles`);
+    })
+    .then(relatedImagePromiseArray=>{
+      relatedImagePromiseArray.map(relatedImage=>{
+        return relatedImage.data.results[0].photos[0].thumbnail_url;
+      });
+    })
+    .then(relatedImageUrlArray=>{
+      this.setState({relatedImages: relatedImageUrlArray});
+    })
+    .catch(err=>{
+      console.log(err);
+    });
+  }
+
+  // componentDidMount() {
+  //   this.buildCarousel();
+  // }
+
+  async componentDidUpdate(prevProps) {
+    //get array of related ID's
     if (this.props.product.id !== prevProps.product.id) {
-      this.getRelatedProduct(this.props.product.id);
+      const relatedIds = await axios.get(`/api/fec2/hrnyc/products/${this.props.product.id}/related`)
+      const ids = relatedIds.data
+      // this.setState({relatedIds: ids});
+
+      //when that's done, get the products
+      const relatedProductsPromises = ids.map(id=>{
+        return axios.get(`/api/fec2/hrnyc/products/${id}`)
+        .then(productPromise=>{
+          return productPromise.data
+        })
+      })
+
+      // const relatedProductsPromises = async() =>{
+      //   let array = [];
+      //   for (const id of ids) {
+      //     array.push(await axios.get(`/api/fec2/hrnyc/products/${id}`)
+      //     .then(productPromise=>{
+      //       return productPromise.data
+      //     }))
+      //   }
+      //   console.log(array)
+      //   return array;
+      // }
+
+      //and then the images
+      const relatedImagesPromises = ids.map(id=>{
+        return axios.get(`/api/fec2/hrnyc/products/${id}/styles`)
+        .then(imagePromise=>{
+          return imagePromise.data.results[0].photos[0].thumbnail_url
+        })
+      })
+
+      // const relatedImagesPromises = async() =>{
+      //   let array = [];
+      //   for (const id of ids) {
+      //     array.push(await axios.get(`/api/fec2/hrnyc/products/${id}/styles`)
+      //     .then(imagePromise=>{
+      //       return imagePromise.data.results[0].photos[0].thumbnail_url
+      //     }))
+      //   }
+      //   console.log(array)
+      //   return array;
+      // }
+
+      //resolve those promises
+      const relatedProducts = await Promise.all(relatedProductsPromises);
+
+      this.setState({
+        relatedProducts: relatedProducts})
+
+      const relatedImages = await Promise.all(relatedImagesPromises);
+
+      //only after they have resolved, store them as state
+      this.setState({
+        relatedImages: relatedImages
+      })
+      // this.getRelatedIds(this.props.product.id);
+      // this.getRelatedProducts(this.state.relatedIds);
+      // this.getRelatedImages(this.state.relatedIds);
     }
     this.buildCarousel();
   }

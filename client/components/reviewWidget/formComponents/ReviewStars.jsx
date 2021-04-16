ba718138a7469b4701b1c4909ecb69e9e2b0a2f8
Colin Chauche <colin.chauche@gmail.com>
2021-04-16 12:30:14 -0400

    Replace input generating loop with shorter map syntax

--- client/components/reviewWidget/formComponents/ReviewStars.jsx
+++ client/components/reviewWidget/formComponents/ReviewStars.jsx
@@ -5,30 +5,13 @@ export default function ReviewStars({ currentValue, handleInputChange }) {
     <>
       <h4>Star Rating</h4>
       <span className="star-cb-group">
-        <input type="radio" id="rating-5" name="star-rating" value="5" />
-        <label htmlFor="rating-5">5</label>
-        <input
-          type="radio"
-          id="rating-4"
-          name="star-rating"
-          value="4"
-          // checked={true}
-        />
-        <label htmlFor="rating-4">4</label>
-        <input type="radio" id="rating-3" name="star-rating" value="3" />
-        <label htmlFor="rating-3">3</label>
-        <input type="radio" id="rating-2" name="star-rating" value="2" />
-        <label htmlFor="rating-2">2</label>
-        <input type="radio" id="rating-1" name="star-rating" value="1" />
-        <label htmlFor="rating-1">1</label>
-        <input
-          type="radio"
-          id="rating-0"
-          name="star-rating"
-          value="0"
-          className="star-cb-clear"
-        />
-        <label htmlFor="rating-0">0</label>
+        {[...Array(5)].map((el, i) => {
+          return (
+            <label key={i}>☆
+              <input type="radio" name="rating" value={i} />
+            </label>
+          );
+        })}
       </span>
     </>
   );

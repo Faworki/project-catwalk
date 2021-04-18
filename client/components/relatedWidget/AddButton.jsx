b94bcae4314b3c7cb3adabc3e6963cd1c4ecad0f
Jimmy Ko <ko.jimmy09@gmail.com>
2021-04-17 21:09:16 -0400

    mocked up add-to-outfit button to look like product card

--- client/components/relatedWidget/AddButton.jsx
+++ client/components/relatedWidget/AddButton.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import StarAverage from '../shared/StarAverage.jsx';
+import ActionButton from './ActionButton.jsx';
 
 class AddButton extends React.Component {
   constructor(props) {
@@ -7,13 +9,20 @@ class AddButton extends React.Component {
 
   render() {
     return (
-      <div className="menu-item-wrapper">
-        <div className="menu-item">
-          <button className="add-button" onClick={this.props.addToOutfit}>
+        <div className="menu-item add-button">
+          <h4>Add To Outfit</h4>
+          <a href>
+            <h4 style={{textDecoration: "underline"}}>Product Name</h4>
+          </a>
+          <button className="outfit-button" onClick={this.props.addToOutfit}>
             +
           </button>
+          <h4>Price</h4>
+          <StarAverage reviewAverage={this.props.stars} />
+          <ActionButton
+            buttonCallback={this.props.buttonCallback}
+          />
         </div>
-      </div>
     );
   }
 }

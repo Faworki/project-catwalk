7bdad8dd8ff485635e55cf57c828a65be54a2c64
Jimmy Ko <ko.jimmy09@gmail.com>
2021-04-19 10:05:29 -0400

    added mannequin icon to add-to-outfit button

--- client/components/relatedWidget/AddButton.jsx
+++ client/components/relatedWidget/AddButton.jsx
@@ -15,7 +15,7 @@ class AddButton extends React.Component {
             <h4 style={{textDecoration: "underline"}}>Add it to your outift!</h4>
           </a>
           <button className="outfit-button" onClick={this.props.addToOutfit}>
-            +
+            <img src='https://www.flaticon.com/svg/vstatic/svg/68/68946.svg?token=exp=1618841571~hmac=351e4ae2373ff549ac3487427ca59eec' width="200" height="250"/>
           </button>
           <h4>See your outfit here!</h4>
           <StarAverage reviewAverage={5} />
